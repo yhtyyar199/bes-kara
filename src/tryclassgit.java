2e2d209e92f7b1196c923ee54a3a2a1cf3e5ca02
yhtyyar199 <68923293+yhtyyar199@users.noreply.github.com>
2020-07-28 22:58:15 +0300

    Update tryclassgit.java

--- src/tryclassgit.java
+++ src/tryclassgit.java
@@ -1,5 +1,6 @@
 public class tryclassgit {
     public static void main(String[] args) {
         System.out.println("git trydan merhaba");
+        System.out.println("github trydan merhaba");
     }
 }
